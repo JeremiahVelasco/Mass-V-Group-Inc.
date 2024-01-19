5304ec7a767d63373d3143585127426641c3d802
Jeremiah Daligues Velasco <velascojeremiahd@gmail.com>
2024-01-19 19:30:29 +0800

    navbar changes and created BatterySuggestion controller

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mass V Group Inc. - Products</title>
+    <link rel="stylesheet" href="index.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="icon" href="assets/MVG Circle 1001 Logo PNG 2 - RGB.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    @section('header')
+        @yield('content')
+        <script src="main.js"></script>
+    </body>
+
+    </html>
--- resources/views/main/products.blade.php
+++ resources/views/main/products.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mass V Group Inc. - Products</title>
+    <title>Mass V Group Inc. - Batteries</title>
     <link rel="stylesheet" href="index.css">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css" rel="stylesheet">
     <link rel="icon" href="assets/MVG Circle 1001 Logo PNG 2 - RGB.png">
