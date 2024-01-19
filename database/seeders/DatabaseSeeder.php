616c474947ac17de153501feff677e6010ce1f9d
KafelnikovDelaRosa <kafelnikovdelarosa@yahoo.com>
2024-01-19 21:09:48 +0800

    Laravel Seeders: Changed it onto for loop to iterate every car model list to fill the table
    To fill in table manufacturers make sure to type php artisan db:seed --class=DatabaseSeeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        Manufacturer::factory(10)->create();
+        foreach(Manufacturer::$manufacturers as $manufactureName){
+            Manufacturer::factory()->create(['name'=>$manufactureName]);
+        }
     }
 }
