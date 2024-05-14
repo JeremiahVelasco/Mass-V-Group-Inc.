303389ac8360edef932be561486887f8dfbf6f04
KafelnikovDelaRosa <kafelnikovdelarosa@yahoo.com>
2024-05-14 13:51:22 +0800

    Main Page: Search functionality fixed

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -70,8 +70,7 @@ class AdminController extends Controller
             return response()->json(['result' => false]);
         }
         
-        // return response()->json(['body'=>$suggested_battery,'mvgi'=>$mvgi,'jis'=>$jis, 'result' => true]);
-        return response()->json($request);
+        return response()->json(['body'=>$suggested_battery,'mvgi'=>$mvgi,'jis'=>$jis, 'result' => true]);
     }
 
     public function products(Request $request) {
